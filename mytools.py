b6bb7f99c96c7e20096a0b0ff4df7b86b63141ca
Lumen1015 <2271039280szr@sina.com>
2023-11-17 23:12:18 +0800

    自动推送

--- mytools.py
+++ mytools.py
@@ -1,6 +1,17 @@
+import pandas as pd
 from pyreadstat import pyreadstat
+import matplotlib.pyplot as plt
+plt.rcParams["font.sans-serif"]=["SimHei"]#设置字体
 
 
-国家认同调查数据表, metadata = pyreadstat.read_sav(R'data\identity.sav',apply_value_formats=True,formats_as_ordered_category=True)
-国家认同调查数据表, metadata = pyreadstat.read_sav(R'data\identity.sav',apply_value_formats=True,formats_as_ordered_category=True)
-result = 国家认同调查数据表['会打多少分'].value_counts(sort=False)
+def 生成有序类别变量描述统计表(数据表路径及文件名，变量):
+                  数据表,metadata = pyreadstat.read_sav(
+                      R'data\identity.sav',apply_value_formats=True,formats_as_ordered_category=True)
+                      df_result = pd.DataFrame(数据表[变量].value_counts(sort=False))
+                      df_result['累计求和'] = df_result['count'].cumsum()
+                      df_result['比例'] = 数据表[变量].value_counts(normalize=True)
+                      df_result['累计比例'] = df_result['比例'].cumsum()
+
+
+
+

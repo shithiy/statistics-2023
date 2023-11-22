f53735461590606903990de05a57f3a44be3ef5b
Lumen1015 <2271039280szr@sina.com>
2023-11-22 12:57:51 +0800

    自动推送

--- mytools.py
+++ mytools.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.sans-serif"]=["SimHei"]#设置字体
 
 
+def 读取SPSS数据文件(文件位置及名称, 是否保留标签值:bool):
+        数据表,metadata = pyreadstat.read_sav(
+                文件位置及名称,apply_value_formats=是否保留标签值,
+                formats_as_ordered_category=True)
+        return 数据表
+
 def 生成有序类别变量描述统计表(表名,变量名):
                       
                       result = 表名[变量名].value_counts(sort=False)

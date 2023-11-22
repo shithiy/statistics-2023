9a8d4776f1a94919d015968a4cd669fac4921f0d
Lumen1015 <2271039280szr@sina.com>
2023-11-22 09:33:21 +0800

    自动推送

--- mytools.py
+++ mytools.py
@@ -4,14 +4,18 @@ import matplotlib.pyplot as plt
 plt.rcParams["font.sans-serif"]=["SimHei"]#设置字体
 
 
-def 生成有序类别变量描述统计表(数据表路径及文件名，变量):
-                  数据表,metadata = pyreadstat.read_sav(
-                      R'data\identity.sav',apply_value_formats=True,formats_as_ordered_category=True)
-                      df_result = pd.DataFrame(数据表[变量].value_counts(sort=False))
-                      df_result['累计求和'] = df_result['count'].cumsum()
-                      df_result['比例'] = 数据表[变量].value_counts(normalize=True)
-                      df_result['累计比例'] = df_result['比例'].cumsum()
+def 生成有序类别变量描述统计表(表名,变量名):
+                      
+                      result = 表名[变量名].value_counts(sort=False)
+                      描述统计表 = pd.DataFrame(result)
+                      描述统计表['比例'] = 描述统计表['count']/ 描述统计表['count'].sum()
+                      描述统计表['累计求和'] = df_result['count'].cumsum()
+                      df_result['比例'] = 描述统计表['比例'].cumsum()
+                      return 描述统计表
 
-
-
-
+def 绘制直方图(表名,y):
+        x = 表名.index
+        y = 表名[y].values
+        fig,ax2 = plt.subplots()
+        ax2.bar(x,y)
+        plt.show()                 
